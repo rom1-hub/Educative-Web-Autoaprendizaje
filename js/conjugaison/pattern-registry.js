b25d61a1b3ea4ad95a4324d762521222285b7f4d
rom1-hub <romain.bouisson@gmail.com>
2026-09-08 19:10:35 +0200

    Add venir-type to central pattern registry

--- js/conjugaison/pattern-registry.js
+++ js/conjugaison/pattern-registry.js
@@ -105,6 +105,26 @@
     return null;
   }
 
+  function venirType(inf,s,tense){
+    const stem=inf.replace(/ir$/,''),singular=stem.replace(/ven$/,'vien'),subjSingular=stem.replace(/ven$/,'vienn'),future=inf.replace(/enir$/,'iendr');
+    if(tense==="présent de l'indicatif"){
+      if(['je','tu','il','elle','on','ils','elles'].includes(s))return singular+{je:'s',tu:'s',il:'t',elle:'t',on:'t',nous:'ons',vous:'ez',ils:'ent',elles:'ent'}[s];
+      return stem+{je:'s',tu:'s',il:'t',elle:'t',on:'t',nous:'ons',vous:'ez',ils:'ent',elles:'ent'}[s];
+    }
+    if(tense==='imparfait')return stem+imparfait[s];
+    if(tense==='futur simple')return future+futur[s];
+    if(tense==='conditionnel présent')return future+conditionnel[s];
+    if(tense==='subjonctif présent'){
+      if(['je','tu','il','elle','on','ils','elles'].includes(s))return subjSingular+subjonctif[s];
+      return stem+subjonctif[s];
+    }
+    if(tense==='impératif présent'){
+      if(!imperativeSubjects.includes(s))return null;
+      return singular+{je:'s',tu:'s',il:'t',elle:'t',on:'t',nous:'ons',vous:'ez',ils:'ent',elles:'ent'}[s];
+    }
+    return null;
+  }
+
   const definitions={
     'regular-er':{groupe:1,description:'Premier groupe régulier en -ER',generate:(inf,s,t)=>simpleGenerator(inf,s,t,'regular-er')},
     'regular-ir':{groupe:2,description:'Deuxième groupe régulier en -IR',generate:(inf,s,t)=>simpleGenerator(inf,s,t,'regular-ir')},
@@ -115,7 +135,8 @@
     'e-accent':{groupe:1,description:'Premier groupe avec alternance E/È',generate:erEAccent},
     'partir-type':{groupe:3,description:'Famille partir : alternance du radical au présent',generate:partirType},
     'suivre-type':{groupe:3,description:'Famille suivre : radical sui-/suiv-',generate:suivreType},
-    'ouvrir-type':{groupe:3,description:'Famille ouvrir : présent en -e/-es/-e, pluriel en -ons/-ez/-ent',generate:ouvrirType}
+    'ouvrir-type':{groupe:3,description:'Famille ouvrir : présent en -e/-es/-e, pluriel en -ons/-ez/-ent',generate:ouvrirType},
+    'venir-type':{groupe:3,description:'Famille venir : alternance vien-/ven- au présent et viendr- au futur',generate:venirType}
   };
 
   function get(pattern){return definitions[pattern]||null;}
