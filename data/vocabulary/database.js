7fc6889bb1bd4044bd2f56346b8d8c14e67313a4
rom1-hub <romain.bouisson@gmail.com>
2026-09-22 21:44:26 +0200

    feat(vocabulario): registrar números cardinales y ordinales

--- data/vocabulary/database.js
+++ data/vocabulary/database.js
@@ -40,6 +40,7 @@
     Object.freeze({id:'time-dates-calendar',title:'Tiempo, fechas y calendario',src:'../data/vocabulary/categories/time-dates-calendar.js?v=20260922-1'}),
     Object.freeze({id:'place-prepositions',title:'Preposiciones de lugar',src:'../data/vocabulary/categories/place-prepositions.js?v=20260922-2'}),
     Object.freeze({id:'avoir-expressions',title:'Expresiones con avoir',src:'../data/vocabulary/categories/avoir-expressions.js?v=20260922-1'}),
+    Object.freeze({id:'numbers-cardinal-ordinal',title:'Números cardinales y ordinales',src:'../data/vocabulary/categories/numbers-cardinal-ordinal.js?v=20260922-1'}),
   ]);
 
   const registry = new Map();
