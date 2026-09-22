57fafab9478f777310c783ee8fb13b38d581a083
rom1-hub <romain.bouisson@gmail.com>
2026-09-23 01:07:51 +0200

    feat(vocabulary): register category 28 common adverbs

--- data/vocabulary/database.js
+++ data/vocabulary/database.js
@@ -42,6 +42,7 @@
     Object.freeze({id:'avoir-expressions',title:'Expresiones con avoir',src:'../data/vocabulary/categories/avoir-expressions.js?v=20260922-1'}),
     Object.freeze({id:'numbers-cardinal-ordinal',title:'Números cardinales y ordinales',src:'../data/vocabulary/categories/numbers-cardinal-ordinal.js?v=20260922-1'}),
     Object.freeze({id:'figurative-idiomatic-expressions',title:'Expresiones figuradas e idiomáticas',src:'../data/vocabulary/categories/figurative-idiomatic-expressions.js?v=20260922-1'}),
+    Object.freeze({id:'common-adverbs',title:'Adverbios más usados',src:'../data/vocabulary/categories/common-adverbs.js?v=20260922-1'}),
   ]);
 
   const registry = new Map();
