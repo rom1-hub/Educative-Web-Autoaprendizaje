7a8735f087b836300c4fedb193944868567e57b4
rom1-hub <romain.bouisson@gmail.com>
2026-09-26 00:58:12 +0200

    Completa las formas conjugadas de s'en

--- scripts/build-local-verb-database.js
+++ scripts/build-local-verb-database.js
@@ -112,7 +112,7 @@ function bareForm(value){
 function reflexivePronoun(index,form,particle){
   const pronouns=['me','te','se','nous','vous','se'];
   const p=pronouns[index]||'se';
-  if(particle==='en')return `${p[0]}'en`;
+  if(particle==='en')return `${p[0]}'en ${form}`;
   return /^[aeiouyàâäéèêëîïôöùûüÿæœh]/i.test(form)?`${p[0]}'${form}`:`${p} ${form}`;
 }
 function pronominalFormList(values,particle){
