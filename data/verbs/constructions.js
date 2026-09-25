2624c987fcd7aa85421d6387652f482dcb0b8d0b
rom1-hub <romain.bouisson@gmail.com>
2026-09-26 00:10:34 +0200

    Add s'acharner to pronominal catalog

--- data/verbs/constructions.js
+++ data/verbs/constructions.js
@@ -15,6 +15,7 @@ window.COQ_PRONOMINAL_RULES=Object.freeze({
   'abstenir':{fonctionDeSe:'aucune',accord:'sujet'},
   'accouder':{fonctionDeSe:'aucune',accord:'sujet'},
   'accroupir':{fonctionDeSe:'aucune',accord:'sujet'},
+  'acharner':{fonctionDeSe:'aucune',accord:'sujet'},
 
   'amuser':{fonctionDeSe:'COD',accord:'sujet'},
   'appeler':{fonctionDeSe:'COD',accord:'sujet'},
