614e57a6a9fba2c993c4b04af1afe259a6cf7443
rom1-hub <romain.bouisson@gmail.com>
2026-09-25 20:49:26 +0200

    Añadir regla de concordancia para se voir

--- data/verbs/constructions.js
+++ data/verbs/constructions.js
@@ -5,6 +5,7 @@
 window.COQ_PRONOMINAL_RULES=Object.freeze({
   'allonger':{fonctionDeSe:'COD',accord:'sujet'},
   'fâcher':{fonctionDeSe:'aucune',accord:'sujet'},
+  'voir':{fonctionDeSe:'COD',accord:'sujet'},
 
   'amuser':{fonctionDeSe:'COD',accord:'sujet'},
   'appeler':{fonctionDeSe:'COD',accord:'sujet'},
